50701b7bfe93b5450df20a857ff7517826ebeed8
Yuri Baburov <burchik@gmail.com>
2020-01-09 20:28:02 +0700

    Experiments with stemming for russian.

---
+++ ru2e/__init__.py
@@ -0,0 +1,31 @@
+# encoding: utf8
+from __future__ import unicode_literals, print_function
+from spacy.lang.ru import RussianDefaults, Russian
+from ru2.lemmatizer import RussianLemmatizer
+from .syntax_iterators import SYNTAX_ITERATORS
+from .tokenizer import StemmingTokenizer
+
+class Russian2Defaults(RussianDefaults):
+    syntax_iterators = SYNTAX_ITERATORS
+    @classmethod
+    def create_lemmatizer(cls, nlp=None):
+        return RussianLemmatizer()
+
+    @classmethod
+    def create_tokenizer(cls, nlp=None):
+        return StemmingTokenizer(nlp.vocab)
+
+
+class Russian2(Russian):
+    lang = 'ru'
+    Defaults = Russian2Defaults
+
+
+def load_ru2(path, exclude=[]):
+    nlp = Russian2()
+    nlp.from_disk(path)
+    nlp.disable_pipes(*exclude)
+    return nlp
+
+
+__all__ = ['Russian2', 'load_ru2']
